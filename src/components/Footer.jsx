e6bf5fe2495db77a10743a1d80da98ede84c4acd
eren500247 <eren500247@gmail.com>
2024-10-07 06:51:39 +0630

    add copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const date = new Date()
+  console.log("Date :",date.getHours())
   return (
     <footer className="text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2">
       <div className="flex items-center justify-center gap-4">
         <Link to={"/"}>About</Link>
         <Link to={"/"}>Contact</Link>
       </div>
-      <p className="text-sm">Create By Name</p>
+      <p className="text-sm">{date.getFullYear()}Eren | All Rights Reserved</p>
     </footer>
   );
 };
